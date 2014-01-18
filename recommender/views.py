8427ee15b94e0abd224356ec540c2fa3f8d0b59c
Amos Kyler <amos.kyler@gmail.com>
2014-01-18 04:23:58 -0800

    Added the rest of the items

--- recommender/views.py
+++ recommender/views.py
@@ -28,16 +28,49 @@ class RecommendView(AjaxView):
         pass
 
 
-boots=Footwear(boots.name = "Boots", boots.gender = 'b', boots.temp_min = None,
-               boots.temp_max = 70, boots.sun = True, boots.snow = True, 
-               boots.rain = True)
+boots=Footwear(name="Boots", gender='b', temp_min=None, temp_max=70, sun=True,
+               snow=True, rain=True)
 
-sandals=Footwear(sandals.name = "Sandals", sandals.gender = 'b', 
-                 sandals.temp_min=75, sandals.temp_max=None, sandals.sun=True, 
-                 sandals.snow=False, sandals.rain = False)
+sandals=Footwear(name="Sandals", gender='b', temp_min=75, temp_max=None, 
+                 sun=True, snow=False, rain = False)
 
-sneakers=Footwear(sneakers.name="Tennis Shoes", sneakers.gender='b', 
-                  sneakers.temp_min=50, sneakers.temp_max=None, 
-                  sneakers.sun = True, sneakers.snow = False,
-                  sneakers.rain = False)
+sneakers=Footwear(name="Tennis Shoes", gender='b', temp_min=50, temp_max=None, 
+                  sun=True, snow=False, rain=False)
+
+umbrella = Accessory(name="Umbrella", gender='b', temp_min=None, temp_max=None,
+                     sun=False, snow=False, rain=True)
+
+warm_hat = Accessories(name="Warm Hat",gender='b', temp_min=None, temp_max=60, 
+                       sun=True, snow=True, rain=False)
+
+scarf = Accessories(name="Scarf", gender='b', temp_min=None, temp_max=60, 
+                    sun=True, snow=False, rain=True)
+
+tshirts = Tops(name="T-Shirt", gender='b', temp_min=65, temp_max=None,
+               sun=True, snow=False, rain=True)
+
+long_shirt = Tops(name="Long-Sleeved Shirt", gender='b', temp_min=None, 
+                  temp_max=75, sun=True, snow=True, rain=True)
+
+short_shirt = Tops(name="Short-Sleeved Shirt", gender='b', temp_min=35, 
+                  temp_max=90, sun=True, snow=True, rain=True)
+
+jeans = Bottoms(name="Jeans", gender='b', temp_min=None, temp_max=95, 
+                sun=True, snow=True, rain=True)
+
+shorts = Bottoms(name="Long Sleeved Shirt", gender='b', temp_min=55, 
+                  temp_max=None, sun=True, snow=False, rain=False)
+
+thermal_underwear = Bottoms(name="Thermal Underwear", gender='b', temp_min=35, 
+                  temp_max=75, sun=True, snow=True, rain=True)
+
+
+heavy_coat = Jackets(name="Heavy Coat", gender='b', temp_min=None, 
+                  temp_max=60, sun=True, snow=True, rain=True)
+
+sweater = Jackets(name="Sweater", gender='b', temp_min=61, 
+                  temp_max=78, sun=True, snow=True, rain=True)
+
+wool_coat = Jackets(name="Heavy Wool Coat", gender='b', temp_min=None, 
+                  temp_max=59, sun=True, snow=True, rain=False)
 
