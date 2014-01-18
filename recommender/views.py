3508dd5a93623ee51b8e202cb54e16b026b07879
omniaura <omniaura5@gmail.com>
2014-01-18 02:11:46 -0600

    created AjaxView CBV

--- recommender/views.py
+++ recommender/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views.generic import View
+from django.http import HttpResponse, Http404
+
+
+class AjaxView(View):
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.is_ajax: 
+            return super(AjaxView, self).dispatch(*args, **kwargs)
+        else:
+            raise Http404
+        
