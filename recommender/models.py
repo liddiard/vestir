a18d361d9a0945bfb28481aedf1cbbaf34cd8eb5
Amos Kyler <amos.kyler@gmail.com>
2014-01-18 02:05:07 -0800

    adding inheritance classes for Clothing

--- recommender/models.py
+++ recommender/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Clothing(object):
+    def __init__(self, gender=none, temp_min=none, temp_max=none, snow=false, sun=false, rain=false):
+        self.gender = gender
+        self.temp_min = temp_min
+        self.temp_max = temp_max
+        self.snow = snow
+        self.sun = sun
+        self.rain = rain
+
+class Footwear(Clothing):
+    def __init__(self, **kwargs):
+        super(Footwear, self).__init__(**kwargs)
+
+
+class Tops(Clothing):
+    def __init__(self, **kwargs):
+        super(Tops, self).__init__(**kwargs)
+
+class Bottoms(Clothing):
+    def __init__(self, **kwargs):
+        super(Bottoms, self).__init__(**kwargs)
+
+class Accessories(Clothing):
+    def __init__(self, **kwargs):
+        super(Accessories, self).__init__(**kwargs)
